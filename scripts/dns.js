e4a452e7ca7ff6b86abccf3ee1bbbfddc634851e
ufiv <15242328+amwufiv@users.noreply.github.com>
2023-07-31 19:39:35 +0800

    Create dns.js
    update

---
+++ scripts/dns.js
@@ -0,0 +1,8 @@
+router_wifi = "ufiv"
+router_dns = "198.8.0.2"
+const doh_servers = "https://120.53.53.53/dns-query,https://1.12.12.12/dns-query,https://223.5.5.5/dns-query,https://223.6.6.6/dns-query"
+if($network.wifi.ssid == router_wifi){
+	$done({server: router_dns})
+} else {
+	$done({servers: doh_servers.split(",")})
+}
