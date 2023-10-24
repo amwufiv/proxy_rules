a6f3e9dea9bca36589050cf08b0b4c78fa46e5d3
ufiv <lingranhuang@gmail.com>
2023-10-25 01:51:13 +0800

    update

--- scripts/sms.js
+++ scripts/sms.js
@@ -1,2 +1,27 @@
-$notification.post("主标题", "副标题", "信息");
+pass = $persistentStore.read("pass")
+body = {
+  pass: pass
+}
+req = {
+  url: 'https://sms.ufiv.org/get',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify(body),
+  timeout: 5
+}
+
+$httpClient.post(req, function (error, response, data) {
+  if (error) {
+    $notification.post("SMS", "查询失败", error);
+  }
+  else if (response.status == 200) {
+    const msglist = JSON.parse(data);
+    msglist.forEach(msg => {
+      $notification.post(msg.sender, msg.time, msg.msg);
+    });
+  } else {
+    $notification.post("SMS", "查询失败:" + response.status, data);
+  }
+})
 $done();
