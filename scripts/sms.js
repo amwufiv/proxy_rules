cd93d848cfdca4a98bce267fd96edd0fcec0af5e
ufiv <lingranhuang@gmail.com>
2023-10-28 18:46:49 +0800

    resolveconflict

--- scripts/sms.js
+++ scripts/sms.js
@@ -13,11 +13,13 @@ req = {
 
 $httpClient.post(req, function (error, response, data) {
   if (error) {
-    // $notification.post("SMS", "查询失败", error);
+    //$notification.post("SMS", "查询失败", error);
   }
   else if (response.status == 200) {
     const msglist = JSON.parse(data);
     msglist.forEach(msg => {
+      // keep last msg
+      $persistentStore.write(JSON.stringify(msg),'last_msg')
       $notification.post(msg.sender, msg.time, msg.msg);
     });
   } else {
